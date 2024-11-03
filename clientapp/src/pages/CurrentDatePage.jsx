4be4be88bb5d72de2dcb51f6256e1c99dfa32a04
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-03 21:13:44 +0000

    Get date,time and timezone

---
+++ clientapp/src/pages/CurrentDatePage.jsx
@@ -0,0 +1,21 @@
+﻿import '../styles/home.css';
+
+const CurrentDatePage = ({ backTo }) => {
+    const date = new Date();
+    const showTime = date.getHours()
+        + ':' + date.getMinutes()
+        + ':' + date.getSeconds();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    return (
+        <div>
+            <h1>Current Time:</h1>
+            <h2>{showTime}</h2>
+
+            <h1>Timezone:</h1>
+            <h2>{timeZone}</h2>
+        </div>
+    )
+}
+
+export default CurrentDatePage;
