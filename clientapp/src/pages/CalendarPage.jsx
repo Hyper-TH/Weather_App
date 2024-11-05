34636396bfe8018c4f5a46dbfb6d25009c9a0d8a
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-05 21:16:43 +0000

    Added calendar values

--- clientapp/src/pages/CalendarPage.jsx
+++ clientapp/src/pages/CalendarPage.jsx
@@ -1,13 +1,18 @@
 ﻿import { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const CalendarPage = ({ backTo }) => {
-    const [value, onChange] = useState(new Date());
+    const [date, setDate] = useState(new Date());
+    const onChange = (newDate) => {
+        setDate(newDate);
+    }
 
     return (
         <>
             <div>
-                <Calendar onChange={onChange} value={value} />
+                <Calendar onChange={onChange} value={date} />
+                <p>Selected date: {date?.toDateString()}</p>
             </div>
         </>
     )
