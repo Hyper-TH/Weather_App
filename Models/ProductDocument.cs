2a60cf50e9f448459e212ef893d4ff17ff0235fc
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-06 23:19:35 +0000

    Added model for Product Document

---
+++ Models/ProductDocument.cs
@@ -0,0 +1,18 @@
+﻿using Google.Cloud.Firestore;
+
+namespace React_ASPNETCore.Models
+{
+    [FirestoreData]
+    public class ProductDocument
+    {
+        [FirestoreDocumentId]
+        public int ID { get; set; }
+
+        [FirestoreProperty]
+        public string Name { get; set; }
+
+        [FirestoreProperty]
+
+        public decimal Price { get; set; }
+    }
+}
