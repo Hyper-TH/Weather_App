05982bee2dece76d8a2b6f17801cb236f974d936
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-10 23:23:41 +0000

    Fixed tailwind yay

--- clientapp/tailwind.config.js
+++ clientapp/tailwind.config.js
@@ -1,11 +1,82 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+];
 
+export const theme = {
+    extend: {
+        colors: {
+            primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+        },
+        fontSize: {
+            'xxs': ['0.625rem', { // 10px font size
+                lineHeight: '0.875rem', // 14px line height
+            }],
+            'xxxs': ['0.5rem', { // Approximately 9.6px font size
+                lineHeight: '0.75rem', // Approximately 12.8px line height
+            }],
+        },
+    },
+    fontFamily: {
+        body: [
+            "Inter",
+            "ui-sans-serif",
+            "system-ui",
+            "-apple-system",
+            "system-ui",
+            "Segoe UI",
+            "Roboto",
+            "Helvetica Neue",
+            "Arial",
+            "Noto Sans",
+            "sans-serif",
+            "Apple Color Emoji",
+            "Segoe UI Emoji",
+            "Segoe UI Symbol",
+            "Noto Color Emoji",
+        ],
+        sans: [
+            "Inter",
+            "ui-sans-serif",
+            "system-ui",
+            "-apple-system",
+            "system-ui",
+            "Segoe UI",
+            "Roboto",
+            "Helvetica Neue",
+            "Arial",
+            "Noto Sans",
+            "sans-serif",
+            "Apple Color Emoji",
+            "Segoe UI Emoji",
+            "Segoe UI Symbol",
+            "Noto Color Emoji",
+        ],
+    },
+    screens: {
+        'mobile_s': '320px',
+
+        'mobile_m': '375px',
+
+        'mobile_l': '425px',
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+    }
+};
+
+export const plugins = [];
