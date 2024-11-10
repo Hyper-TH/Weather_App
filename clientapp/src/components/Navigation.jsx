d097cea0378d5016f4d931fbea0f9d7ba2ee5131
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-10 23:25:21 +0000

    Fixed navigation

--- clientapp/src/components/Navigation.jsx
+++ clientapp/src/components/Navigation.jsx
@@ -1,24 +1,25 @@
 ﻿import { Link } from 'react-router-dom';
-import '../styles/home.css';
 
-const Navigation = (() => {
-    <>
-        <h2 className="sub_title">
-            User Login in the future
-        </h2>
+const Navigation = () => { 
+    return (
+        <>
+            <h2 className="sub_title">
+                User Login in the future
+            </h2>
 
-        <Link to="/weather" className="btn_collection_top">
-            See the weather today
-        </Link>
+            <Link to="/weather" className="btn_collection_top">
+                See the weather today
+            </Link>
 
-        <Link to="/products" className="btn_collection_middle">
-            See the list of products
-        </Link>
+            <Link to="/products" className="btn_collection_middle">
+                See the list of products
+            </Link>
 
-        <Link to="/datetime" className="btn_collection_bottom">
-            See the date and time today
-        </Link>
-    </>
-})
+            <Link to="/datetime" className="btn_collection_bottom">
+                See the date and time today
+            </Link>
+        </>
+    );
+}
 
 export default Navigation;
