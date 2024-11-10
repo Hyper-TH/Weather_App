45463ffc4acd712b75da7a1c137d292fb8be5fef
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-10 22:36:34 +0000

    Updated ProductDocument model

--- Models/ProductDocument.cs
+++ Models/ProductDocument.cs
@@ -5,14 +5,15 @@ namespace React_ASPNETCore.Models
     [FirestoreData]
     public class ProductDocument
     {
-        [FirestoreDocumentId]
-        public int ID { get; set; }
+
+        [FirestoreProperty]     // Manual 
+        public int ProductID { get; set; }
 
         [FirestoreProperty]
         public string Name { get; set; }
 
         [FirestoreProperty]
 
-        public decimal Price { get; set; }
+        public double Price { get; set; }
     }
 }
